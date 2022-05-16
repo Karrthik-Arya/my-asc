56bfe94c547ae7aa1fa363d2ae52e51b3aba1ada
Karrthik Arya <200020068@iitb.ac.in>
2022-05-17 01:22:03 +0530

    tree update

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -13,6 +13,25 @@ function Sidebar(){
             <div className="tree">
 
             </div>
+            <p style={{marginLeft:"6px", marginTop:"5px", fontSize:"0.75vw"}}>
+            <img src="assets/tp.bmp" />Academic</p>
+            <p style={{marginLeft:"6px",marginTop:"-15px", fontSize:"0.75vw"}}>
+            <img src="assets/tp.bmp" />
+            Library/Archives</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tn.bmp" /> Placement</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tn.bmp" />Moodle</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tn.bmp" />IRCC</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tp.bmp" />Admin Interfaces</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tn.bmp" />Surveys</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tn.bmp" />Maintenance complaints</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tn.bmp" />Guest House</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tn.bmp" />Visitor Entry/Exit Pass</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tn.bmp" />GatePass</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tp.bmp" />Downloadable Forms</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tn.bmp" />Online Purchase System (Reports Only)</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tn.bmp" />Scrutiny of job applicants</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tn.bmp" />Online Mobile Registry</p>
+            <p style={{marginLeft:"6px", fontSize:"0.75vw"}}><img src="assets/tn.bmp" />Print</p>
         </div>
     );
 }
